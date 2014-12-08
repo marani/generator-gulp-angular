94969f0011bb92b554ab24fbc5480f4099b641bb
TMD <dung1@e.ntu.edu.sg>
2014-12-08 08:29:37 +0800

    Add no sandbox args for chrome.

--- app/templates/protractor.conf.js
+++ app/templates/protractor.conf.js
@@ -6,7 +6,10 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['no-sandbox=true']
+    }
   },
 
   // Spec patterns are relative to the current working directly when
