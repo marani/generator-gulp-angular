02c894780b314a92723482bfec3cd5ce6da0c6a1
TMD <dung1@e.ntu.edu.sg>
2014-12-26 14:56:25 +0800

    Move replace prefix to utils. Change _build.js

--- app/src/utils.js
+++ app/src/utils.js
@@ -1,11 +1,33 @@
 'use strict';
 
 var path = require('path');
+var _ = require('lodash');
 
-function normalizePath (str) {
+function normalizePath(str) {
   return path.relative(process.cwd(), path.join(process.cwd(), str));
 }
 
-module.exports = {
-  normalizePath: normalizePath
+/**
+ * Replace sourceFolder with destFolder in filePath
+ * if filePath has any sourceFolder as prefix
+ * @param  {String} filePath    File path to be altered
+ * @param  {Object} folderPairs Hash of pairs of sourceFolder:destFolder
+ *                              Similar to what stored in this.props.paths
+ * @return {String}             new file path
+ */
+function replacePrefix(filePath, folderPairs) {
+  var result = filePath;
+  _.some(folderPairs, function(destFolder, sourceFolder) {
+    if (filePath.indexOf(sourceFolder) === 0) {
+      result = filePath.replace(sourceFolder, destFolder);
+      return true;
+    }
+    return false;
+  });
+  return result;
 }
+
+module.exports = {
+  normalizePath: normalizePath,
+  replacePrefix: replacePrefix
+};
--- app/src/write.js
+++ app/src/write.js
@@ -2,26 +2,15 @@
 
 var files = require('../files.json');
 var path = require('path');
+var utils = require('./utils');
 
 /* Process files */
 module.exports = function () {
   var _ = this._;
 
-  // Replace sourceFolder with destFolder in destination path
-  // folderPairs is a dictionary of source:dest similar to what 
-  // stored in this.props.paths
-  function applyPathConfig(filePath, folderPairs) {
-    var result = filePath;
-    _.forEach(folderPairs, function(destFolder, sourceFolder) {
-      if (filePath.indexOf(sourceFolder) === 0)
-        result = filePath.replace(sourceFolder, destFolder);
-    });
-    return result;
-  }
-
   // Copy static files
   _.forEach(files.staticFiles, function(src) {
-    var dest = applyPathConfig(src, this.props.paths);
+    var dest = utils.replacePrefix(src, this.props.paths);
     this.fs.copy(this.templatePath(src),  this.destinationPath(dest));
   }.bind(this));
 
@@ -32,19 +21,19 @@ module.exports = function () {
 
   // Copy files formatted (format.js) with options selected in prompt
   _.forEach(this.technologiesLogoCopies, function(src) {
-    var dest = applyPathConfig(src, this.props.paths);
+    var dest = utils.replacePrefix(src, this.props.paths);
     this.fs.copy(this.templatePath(src),  this.destinationPath(dest));
   }.bind(this));
   _.forEach(this.partialCopies, function(value, key) {
-    var dest = applyPathConfig(value, this.props.paths);
+    var dest = utils.replacePrefix(value, this.props.paths);
     this.fs.copy(this.templatePath(key),  this.destinationPath(dest));
   }.bind(this));
   _.forEach(this.styleCopies, function(value, key) {
-    var dest = applyPathConfig(value, this.props.paths);
+    var dest = utils.replacePrefix(value, this.props.paths);
     this.fs.copy(this.templatePath(key),  this.destinationPath(dest));
   }.bind(this));
   _.forEach(this.srcTemplates, function(value, key) {
-    var dest = applyPathConfig(value, this.props.paths);
+    var dest = utils.replacePrefix(value, this.props.paths);
     this.template(key, dest);
   }.bind(this));
   _.forEach(this.lintConfCopies, function(src) {
@@ -57,7 +46,7 @@ module.exports = function () {
   _.forEach(files.templates, function(dest) {
     basename = path.basename(dest);
     src = dest.replace(basename, '_' + basename);
-    dest = applyPathConfig(dest, this.props.paths);
+    dest = utils.replacePrefix(dest, this.props.paths);
     this.fs.copyTpl(this.templatePath(src),  this.destinationPath(dest), this);
   }.bind(this));
 };
